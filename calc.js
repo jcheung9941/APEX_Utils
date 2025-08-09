d5f03e0510eb12d8bcb935b3746187314d318cf0
jcheung9941 <75763997+jcheung9941@users.noreply.github.com>
2025-08-08 23:57:14 -0400

    Initial

---
+++ calc.js
@@ -0,0 +1,35 @@
+let baseFactor = [1,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10]
+let baseValue = {BBH:24,BDE:24,BSE:12,MCG:300,TRU:20}
+
+function numCheck(field) {
+    let input = document.getElementsByName(field)[0]
+    let start = document.getElementsByName('Start')[0]
+    let end = document.getElementsByName('End')[0]
+    if(+input.value < 0) {
+        input.value = 0
+    }
+    if(+input.value > 20) {
+        input.value = 20
+    }
+    if(+start.value >= +end.value) {
+        end.value = +start.value + 1
+    }
+    document.querySelector('.startCap').innerText = Math.ceil(+start.value / 2)*500
+    document.querySelector('.endCap').innerText = Math.ceil(+end.value / 2)*500
+    let factor = 0
+    console.log('start',start.value)
+    console.log(factor)
+    for(let i = +start.value; i < baseFactor.length; i++){
+        console.log('i',i)
+        if(i < end.value){
+            factor += baseFactor[i]
+        } else {i += 100}
+        console.log('plus',baseFactor[i])
+        console.log(factor)
+    }
+    document.querySelectorAll('th').forEach((element)=>{
+        if(element.id){
+            element.innerText = factor * baseValue[element.id]
+        }
+    })
+}
